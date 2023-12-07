7649239e8994456bfe3ffa028b92e72623b2b8f2
ninacnel <nerinacoronel00@gmail.com>
2023-12-07 15:37:04 -0300

    Handle the not found product

--- src/components/assets/NotFound.js
+++ src/components/assets/NotFound.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <div className="d-flex justify-content-center align-items-center flex-column mt-5">
+      <h6>Producto no encontrado.</h6>
+      <img src="https://i.pinimg.com/originals/41/10/e7/4110e78f8efa1472bc2fef953e9c71cd.png" />
+    </div>
+  );
 };
 
 export default NotFound;
--- src/components/productPage/ProductPage.js
+++ src/components/productPage/ProductPage.js
@@ -1,51 +1,55 @@
 import React, { useEffect, useState } from "react";
 import ProductCard from "../productCard/ProductCard";
+import NotFound from "../assets/NotFound";
 
 const ProductPage = () => {
   const [item, setItem] = useState({});
   const [id, setId] = useState(0);
-
-  useEffect(() => {
-    const url = window.location.href;
-
-    const match = url.match(/\/prod\/(\d+)/);
-
-    // hace un array con la url y se queda con lo que hay en la posicion 1
-    if (match && match[1]) {
-      setId(match[1]);
-    }
-  }, []);
+  const [notFound, setNotFound] = useState(false);
 
   const urlApi = `https://fakestoreapi.com/products/${id}`;
 
   useEffect(() => {
-    fetch(urlApi)
-      .then((res) => {
-        if (!res.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return res.json();
-      })
-      .then((json) => {
-        if (Object.keys(json).length > 0) {
-          // para ver si la response viene vacía
-          setItem(json);
-        } else {
-          throw new Error("ERROR JSON response vacía");
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-        // Error al pegarle a la api
-      });
-  }, [id]);
+    const url = window.location.href;
+    const match = url.match(/\/prod\/(\d+)/);
+
+    if (match && match[1]) {
+      const productId = match[1];
+
+      const urlApi = `https://fakestoreapi.com/products/${productId}`;
+
+      fetch(urlApi)
+        .then((res) => {
+          const contentLength = res.headers.get("content-length");
+          if (!res.ok) {
+            throw new Error("Network response was not ok");
+          } else if (contentLength && parseInt(contentLength, 10) === 0) {
+            setNotFound(true);
+            throw new Error("Empty response");
+          } //la response puede ser ok 200 pero venir vacia igual
+
+          return res.json();
+        })
+        .then((json) => {
+          if (Object.keys(json).length > 0) {
+            setItem(json);
+          } else {
+            setNotFound(true);
+            throw new Error("ERROR JSON response vacía");
+          }
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    }
+  }, []);
 
   return (
     <div style={{ marginTop: "50px" }}>
       <div className="container">
         <div className="row">
           <div className="col-md-4 offset-md-4">
-            <ProductCard item={item} />
+            {notFound ? <NotFound /> : <ProductCard item={item} />}
           </div>
         </div>
       </div>
